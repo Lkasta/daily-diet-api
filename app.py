ae3f38e5d3e9d1ab81f9ae75b3a48352ac6a442b
Lkasta <lkastabackup@gmail.com>
2025-07-07 21:29:40 -0300

    feat(api): added POST /register-diet route

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import os
 from dotenv import load_dotenv
 
 from database import db
-from flask import Flask
+from flask import Flask,jsonify, request
+from datetime import datetime
 
 from models.diet import Diet
 
@@ -16,6 +17,28 @@ db.init_app(app)
 @app.route("/", methods=['GET'])
 def hello_jonas():
  return "Jonas"
+
+@app.route('/register-diet', methods=['POST'])
+def register_diet():
+  data = request.json
+  name = data.get("name")
+  description = data.get("description")
+  within_diet = data.get("within_diet") or False
+  date_time = data.get("date_time") or datetime.now()
+
+  if name and description:
+    diet = Diet(
+      name=name,
+      description=description, 
+      date_time=date_time,
+      within_diet=within_diet
+    )
+    db.session.add(diet)
+    db.session.commit()
+
+    return jsonify({"message": f"{name} added susccessfully"})
+  
+  return jsonify({"message": "Invalid credentials"}), 400
 
 if __name__ == '__main__':
  app.run(debug=True)
--- models/diet.py
+++ models/diet.py
@@ -1,9 +1,9 @@
 from database import db
-from datetime import datetime
+from sqlalchemy.sql import func
 
 class Diet(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(80), nullable=False)
-  description = db.Column(db.String(300), default=datetime.utcnow)
+  description = db.Column(db.String(300), nullable=False)
   date_time = db.Column(db.DateTime, nullable=False)
-  within_diet = db.Column(db.Boolean, default=False )
+  within_diet = db.Column(db.Boolean, default=False)
